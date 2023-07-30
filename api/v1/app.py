dc3d9d5d045cf6d3b88c6dfa23bad0ddbd4293a0
Cyril-777 <kyrillosmaher10@gmail.com>
2023-07-30 17:31:40 +0000

    api

---
+++ api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""the api application"""
+from flask import Flask, make_response, jsonify
+from os import getenv
+from api.v1.views import app_views
+from models import storage
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
+CORS(app, resources={'/*': {'origins': app_host}})
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """Closes the storage on teardown."""
+    storage.close()
+
+@app.errorhandler(404)
+def error_404(error):
+    """404 page not found handling"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
+
+    app.run(host=host, port=port, threaded=True)
